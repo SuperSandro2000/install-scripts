05a6b8672c87328c1c8918a6cb0016a871a6b3e1
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-08-27 08:57:40 +0200

    Cleanup

--- programs/ttyd.sh
+++ programs/ttyd.sh
@@ -3,7 +3,7 @@ set -eoux pipefail
 
 sudo apt-get install -qy build-essential cmake git libjson-c-dev libwebsockets-dev pkg-config
 cd "$HOME/src/"
-git clone https://github.com/tsl0922/ttyd.git || cd ttyd && git fetch origin && git reset --hard origin/master && cd ..
+git clone https://github.com/tsl0922/ttyd.git || (cd ttyd && git fetch origin && git reset --hard origin/master)
 mkdir -p ttyd/build && cd ttyd/build
 cmake ..
 make
