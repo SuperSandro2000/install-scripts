e2eb89c46fe0ff31f716b61cb9250f7080e65a0d
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-10-12 15:45:53 +0200

    Mkdir just incase

--- programs/ttyd.sh
+++ programs/ttyd.sh
@@ -2,6 +2,7 @@
 set -eoux pipefail
 
 sudo apt-get install -qy build-essential cmake git libjson-c-dev libwebsockets-dev pkg-config
+mkdir -p "$HOME/src/"
 cd "$HOME/src/"
 git clone https://github.com/tsl0922/ttyd.git || (cd ttyd && git fetch origin && git reset --hard origin/master)
 mkdir -p ttyd/build && cd ttyd/build
