4e0856e50b47dfc6c32826d3a126358b07467ce1
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-10-29 21:29:36 +0100

    Add arp-scan

---
+++ programs/arp-scan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eoux pipefail
+
+sudo apt-get install -qy build-essential git libcap-dev libsqlite3-dev
+mkdir -p "$HOME/src/"
+cd "$HOME/src/"
+git clone git@github.com:royhills/arp-scan.git || (cd arp-scan && git fetch origin && git reset --hard origin/master)
+cd arp-scan || exit 1
+autoreconf --install
+./configure
+make
+make check
+sudo make install
