38409370c934ce8b7be07dd0ae68f1d174402c5f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-11-10 11:06:36 +0100

    Fix shellcheck

--- programs/shellcheck.sh
+++ programs/shellcheck.sh
@@ -4,7 +4,7 @@ set -eoux pipefail
 file="shellcheck-stable.linux.x86_64.tar.xz"
 
 sudo apt-get install -qy tar wget
-wget https://storage.googleapis.com/shellcheck/$shellcheck
+wget https://storage.googleapis.com/shellcheck/$file
 tar -xf $file
 rm $file
 sudo mv shellcheck-stable/shellcheck /usr/bin/shellcheck
