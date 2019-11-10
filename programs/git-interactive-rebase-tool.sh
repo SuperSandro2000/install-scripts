b110f49351a250c9d61b1b413fb36068c93aa8c3
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-11-10 04:47:41 +0100

    Add git interactive rebase tool

---
+++ programs/git-interactive-rebase-tool.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eoux pipefail
+
+version=1.1.0
+file=git-interactive-rebase-tool_${version}_amd64.deb
+
+sudo apt-get install -qy libncursesw5 wget
+wget https://github.com/MitMaro/git-interactive-rebase-tool/releases/download/$version/$file
+sudo apt-get install ./$file
+rm $file
