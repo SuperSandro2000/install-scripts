a87f2c82cb8671fc88070e55c78d01e66625098c
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-11-12 12:37:54 +0100

    Make shfmt executable

--- programs/shfmt.sh
+++ programs/shfmt.sh
@@ -6,3 +6,4 @@ file=shfmt_v${version}_linux_amd64
 
 sudo apt-get install -qy wget
 wget https://github.com/mvdan/sh/releases/download/v$version/$file -O "$HOME/.local/bin/shfmt"
+chmod +x "$HOME/.local/bin/shfmt"
