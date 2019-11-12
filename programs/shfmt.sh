66122f6fb908ddb034d621907199a8c26cf23131
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-11-12 12:36:17 +0100

    Add shfmt

---
+++ programs/shfmt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eoux pipefail
+
+version=2.6.4
+file=shfmt_v${version}_linux_amd64
+
+sudo apt-get install -qy wget
+wget https://github.com/mvdan/sh/releases/download/v$version/$file -O "$HOME/.local/bin/shfmt"
