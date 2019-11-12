88b55e2efd954af793bfa3437343011e34f72b18
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-11-12 08:41:16 +0100

    Activate GIRP

--- programs/git-interactive-rebase-tool.sh
+++ programs/git-interactive-rebase-tool.sh
@@ -8,3 +8,4 @@ sudo apt-get install -qy libncursesw5 wget
 wget https://github.com/MitMaro/git-interactive-rebase-tool/releases/download/$version/$file
 sudo apt-get install ./$file
 rm $file
+git config --global sequence.editor interactive-rebase-tool
