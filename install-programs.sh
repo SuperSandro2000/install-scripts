2d9ff6dec61c8f4f3ab578f8f087204d771aa353
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-11-25 19:32:31 +0100

    Move random utils to meta package

--- install-programs.sh
+++ install-programs.sh
@@ -6,9 +6,6 @@ for f in programs/*.sh; do
   bash "$f" -H
 done
 
-# random utils
-sudo apt install -qy curl git git-extras gpg htop mailutils tree unzip zip
-
 # expand cron path
 (crontab -l 2>/dev/null; echo -e "SHELL=/bin/bash\nPATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin")| crontab -
 (sudo crontab -l 2>/dev/null; echo -e "SHELL=/bin/bash\nPATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin")| sudo crontab -
