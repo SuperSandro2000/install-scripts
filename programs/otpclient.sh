82557406c3441eb7a93be6562f9c7296fcd77ebc
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-12-24 16:42:57 +0100

    Add otpclient

---
+++ programs/otpclient.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -eoux pipefail
+
+_git() {
+  dir=$(echo "$1" | \grep -oP '\w+(?=.git)')
+  git clone "$1" || (cd "$dir" && git fetch origin && git reset --hard origin/master)
+  cd "$dir"
+}
+
+_make() {
+  mkdir -p build
+  cd build
+  cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr ../
+  make
+  sudo make install
+}
+
+libbaseencode=1.0.9
+libctop=1.2.2
+otpclient=2.0.1
+
+sudo apt-get install -qy cmake git
+mkdir -p "$HOME/src" && cd "$HOME/src/"
+
+# libbaseencode
+_git https://github.com/paolostivanin/libbaseencode.git
+git checkout v$libbaseencode
+_make
+
+# libcotp
+_git https://github.com/paolostivanin/libcotp.git
+git checkout v$libctop
+_make
+
+# otpclient
+#sudo apt-get install -qy libgcrypt20-dev libjansson-dev libpng-dev libzip-dev
+sudo apt-get install -qy libzbar-dev
+_git https://github.com/paolostivanin/OTPClient.git
+git checkout v$otpclient
+_make
