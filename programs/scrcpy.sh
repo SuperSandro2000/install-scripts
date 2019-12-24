dbacb1a0d594264a8eebbd8d285647f85188935b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2019-12-24 16:42:29 +0100

    Scrcpy: Remove hardcoded version number

--- programs/scrcpy.sh
+++ programs/scrcpy.sh
@@ -10,7 +10,7 @@ sudo apt-get install -qy build-essential fastjar git libavformat-dev libsdl2-dev
 mkdir -p "$HOME/src" && cd "$HOME/src/"
 git clone https://github.com/Genymobile/scrcpy.git || (cd scrcpy && git fetch origin && git reset --hard origin/master)
 cd scrcpy
-git checkout v1.11
+git checkout v$scrcpy
 wget "https://github.com/Genymobile/scrcpy/releases/download/v$scrcpy/scrcpy-server-v$scrcpy"
 echo "ff3a454012e91d9185cfe8ca7691cea16c43a7dcc08e92fa47ab9f0ea675abd1 *scrcpy-server-v$scrcpy" | sha256sum -c -
 meson x --buildtype release --strip -Db_lto=true "-Dprebuilt_server=scrcpy-server-v$scrcpy"
