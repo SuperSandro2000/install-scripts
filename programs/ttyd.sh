cd5dafd9a34f1833d087f683870c7f15c102a89f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-02-05 17:05:47 +0100

    TTYD: Clone shallow

--- programs/ttyd.sh
+++ programs/ttyd.sh
@@ -4,7 +4,7 @@ set -eoux pipefail
 sudo apt-get install --no-install-recommends -qy ca-certificates cmake g++ git make pkg-config libjson-c-dev libwebsockets-dev
 mkdir -p "$HOME/src/"
 cd "$HOME/src/"
-git clone https://github.com/tsl0922/ttyd.git || (cd ttyd && git fetch origin && git reset --hard origin/master)
+git clone --depth=1 https://github.com/tsl0922/ttyd.git || (cd ttyd && git fetch origin && git reset --hard origin/master)
 mkdir -p ttyd/build && cd ttyd/build
 cmake ..
 make
