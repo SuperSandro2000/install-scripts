21bc81befd771c0d82fd74a343142ba1ec214455
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-02-09 07:27:09 +0100

    Shellcheck: Only use sudo when not uid 0

--- programs/shellcheck.sh
+++ programs/shellcheck.sh
@@ -5,8 +5,14 @@ file="shellcheck-stable.linux.x86_64.tar.xz"
 
 trap 'rm $file' EXIT
 
-sudo apt-get install --no-install-recommends -qy ca-certificates tar xz-utils wget
+if [[ $(id -u) != 0 ]]; then
+  sudo=sudo
+else
+  sudo=
+fi
+
+$sudo apt-get install --no-install-recommends -qy ca-certificates tar xz-utils wget
 wget https://storage.googleapis.com/shellcheck/$file
 tar -xf $file
-sudo mv shellcheck-stable/shellcheck /usr/bin/shellcheck
+$sudo mv shellcheck-stable/shellcheck /usr/bin/shellcheck
 rm -r shellcheck-stable
