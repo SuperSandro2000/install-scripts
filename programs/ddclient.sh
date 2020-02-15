ca489a4d7e4125c0b43b7c21f46e530d80ff0aa9
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-02-15 01:22:51 +0100

    Remove duplicate cd

--- programs/ddclient.sh
+++ programs/ddclient.sh
@@ -10,6 +10,7 @@ fi
 $sudo apt-get install --no-install-recommends -qy ddclient git perl libdata-validate-ip-perl libjson-any-perl
 mkdir -p "$HOME"/src
 cd "$HOME"/src
-git clone https://github.com/ddclient/ddclient.git && cd ddclient || cd ddclient && git fetch origin && git reset --hard origin/master
+git clone https://github.com/ddclient/ddclient.git || (cd ddclient && git fetch origin && git reset --hard origin/master)
+cd ddclient
 $sudo cp ddclient /usr/sbin/
 $sudo mkdir -p /etc/ddclient/
