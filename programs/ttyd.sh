40cafe436eb4381a500d98ac5664f781e61b1615
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-06-16 12:18:47 +0200

    ttyd: only require gcc

--- programs/ttyd.sh
+++ programs/ttyd.sh
@@ -7,7 +7,7 @@ else
   sudo=
 fi
 
-$sudo apt-get install --no-install-recommends -qy ca-certificates cmake g++ git make libjson-c-dev libwebsockets-dev
+$sudo apt-get install --no-install-recommends -qy ca-certificates cmake gcc git make libjson-c-dev libwebsockets-dev
 mkdir -p "$HOME/src/"
 cd "$HOME/src/"
 git clone --depth=1 https://github.com/tsl0922/ttyd.git || (cd ttyd && git fetch origin && git reset --hard origin/master)
